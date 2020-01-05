1c0d55b85698b766667d7af8383a5f58be776c24
setuidroot <32213080+setuidroot@users.noreply.github.com>
2020-01-05 08:18:05 -0600

    Update build.bat
    Fix merge conflict merging latest xmrigMO into xmrig-dev

--- misc/build.bat
+++ misc/build.bat
@@ -1,5 +1,5 @@
 @echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 rmdir /S /Q build
 del %~dp0\xmrig-%1-win64.zip
 mkdir build &&^
@@ -9,10 +9,11 @@ git clone https://github.com/xmrig/xmrig-deps.git &&^
 mkdir xmrig\build &&^
 cd xmrig\build &&^
 git checkout %1 &&^
-cmake .. -G "Visual Studio 15 2017 Win64" -DXMRIG_DEPS=%~dp0\build\xmrig-deps\msvc2017\x64 &&^
+cmake .. -G "Visual Studio 16 2019" -DXMRIG_DEPS=%~dp0\build\xmrig-deps\msvc2019\x64 &&^
 msbuild /p:Configuration=Release xmrig.sln &&^
 cd Release &&^
 copy ..\..\src\config.json . &&^
-7z a -tzip -mx %~dp0\xmrig-%1-win64.zip xmrig.exe config.json &&^
+copy ..\..\bin\WinRing0\WinRing0x64.sys . &&^
+7z a -tzip -mx %~dp0\xmrig-%1-win64.zip xmrig.exe config.json WinRing0x64.sys &&^
 cd %~dp0 &&^
 rmdir /S /Q build
